269cf4c44a814ac3f600eb7eea872a8a92ef21d5
unknown <danusenko@gmail.com>
2021-05-21 18:14:56 +0300

    added gun and hp for enemies, added animation flip cucumber

---
+++ Assets/Scripts/Shooter.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour
+{
+    [SerializeField] GameObject projectilePrefab;
+    [SerializeField] GameObject gun;
+    [SerializeField] Animator animator;
+    [SerializeField] int damage;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //fire();
+    }
+
+    private void OnTriggerEnter2D(Collider2D otherCollider)
+    {
+        Health enemyHealth = otherCollider.GetComponent<Health>();
+        Enemy enemy = otherCollider.GetComponent<Enemy>();
+        if (enemyHealth && enemy)
+        {
+            enemyHealth.DealDamage(damage);
+            Destroy(gameObject);
+        }
+    }
+
+    public void Fire()
+    {
+        var projectile = Instantiate(projectilePrefab, gun.transform.position, Quaternion.identity);
+    }
+}
